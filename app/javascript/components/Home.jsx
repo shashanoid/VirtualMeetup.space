cd9083b35d1a8e04f7f9d9f6e2ffc5aa7af3fdcf
Shashwat Singh <shashanoid@gmail.com>
2020-03-28 13:10:38 -0400

    First Virtual Meetup

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Virtual Meetup</h1>
+        <hr className="my-4" />
+        <Link
+          to="/recipes"
+          className="btn btn-lg custom-button"
+          role="button"
+        >
+          Nice
+        </Link>
+      </div>
+    </div>
+  </div>
+);
